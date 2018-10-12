f7cf29f2e98cc5522f66d80a0a71f48c04e3347a
mengxiaofei <xiaofei.meng_tic@baozun.com>
2018-10-12 09:19:46 +0800

    完善添加用户

--- src/config/index.js
+++ src/config/index.js
@@ -13,8 +13,8 @@ export default {
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/',
-    // dev: 'http://localhost:3400/v1/blog/',
+    // dev: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/',
+    dev: 'http://localhost:3400/v1/blog/',
     pro: 'https://produce.com'
   }
 }
--- src/view/users/index.vue
+++ src/view/users/index.vue
@@ -17,16 +17,31 @@
       <div v-show="step === 0" class="role">
         <Row type="flex" justify="center">
           <Col span="6">
-            <img src="../../assets/images/role.jpg" alt="普通用户">
-            <p>我是普通用户</p>
+            <div class="item" @click="choserole(0)">
+              <img src="../../assets/images/role.jpg" alt="普通用户">
+              <p>我是普通用户</p>
+              <div class="mask" :class="{active:choseindex == 0}">
+                <Icon type="md-checkmark-circle-outline" />
+              </div>
+            </div>
           </Col>
           <Col span="6">
-            <img src="../../assets/images/role.jpg" alt="管理员">
-            <p>我是管理员</p>
+            <div class="item" @click="choserole(1)">
+              <img src="../../assets/images/role.jpg" alt="管理员">
+              <p>我是管理员</p>
+              <div class="mask" :class="{active:choseindex == 1}">
+                <Icon type="md-checkmark-circle-outline" />
+              </div>
+            </div>
           </Col>
           <Col span="6">
-            <img src="../../assets/images/role.jpg" alt="超级管理员">
-            <p>我是超级管理员</p>
+            <div class="item" @click="choserole(2)">
+              <img src="../../assets/images/role.jpg" alt="超级管理员">
+              <p>我是超级管理员</p>
+              <div class="mask" :class="{active:choseindex == 2}">
+                <Icon type="md-checkmark-circle-outline" />
+              </div>
+            </div>
           </Col>
         </Row>
         <Row type="flex" justify="end">
@@ -35,8 +50,82 @@
           </Col>
         </Row>
       </div>
-      <div v-show="step === 1">
-        基本信息
+      <div v-show="step === 1" class="information">
+        <div style="margin-top: 6px">
+          <Row class="field">
+            <Col span="8">
+              <p>用户名：</p>
+            </Col>
+            <Col span="16">
+              <Input placeholder="请输入用户名" style="width: auto"></Input>
+            </Col>
+          </Row>
+          <Row class="field">
+            <Col span="8">
+              <p>用户昵称：</p>
+            </Col>
+            <Col span="16">
+              <Input placeholder="请输入用户昵称" style="width: auto"></Input>
+            </Col>
+          </Row>
+          <Row class="field">
+            <Col span="8">
+              <p>头像：</p>
+            </Col>
+            <Col span="16">
+              <Input placeholder="请输入用户名" style="width: auto"></Input>
+            </Col>
+          </Row>
+          <Row class="field">
+            <Col span="8">
+              <p>性别：</p>
+            </Col>
+            <Col span="16">
+              <RadioGroup v-model="information.sex">
+                <Radio label="保密"></Radio>
+                <Radio label="男"></Radio>
+                <Radio label="女"></Radio>
+              </RadioGroup>
+            </Col>
+          </Row>
+          <Row class="field">
+            <Col span="8">
+              <p>生日：</p>
+            </Col>
+            <Col span="16">
+              <DatePicker
+                      :open="open"
+                      :value="information.birthday"
+                      confirm
+                      type="date"
+                      @on-change="changebirthday"
+                      @on-clear="open = false"
+                      @on-ok="open = false">
+                      <a href="javascript:void(0)" @click="chosebirthday">
+                      <Icon type="ios-calendar-outline"></Icon>
+                      <template v-if="information.birthday === ''">请选择日期</template>
+                      <template v-else>{{ information.birthday }}</template>
+                  </a>
+              </DatePicker>
+            </Col>
+          </Row>
+          <Row class="field">
+            <Col span="8">
+              <p>个性签名：</p>
+            </Col>
+            <Col span="16">
+              <Input placeholder="请输入个性签名" style="width: auto"></Input>
+            </Col>
+          </Row>
+          <Row class="field">
+            <Col span="8">
+              <p>个人网址：</p>
+            </Col>
+            <Col span="16">
+              <Input placeholder="请输入个人网址" style="width: auto"></Input>
+            </Col>
+          </Row>
+        </div>
         <Button type="info" @click="step += 1">下一步</Button>
       </div>
     </Modal>
@@ -51,7 +140,13 @@ export default {
       columns: [],
       datas: [],
       showmodal: false,
-      step: 0
+      step: 0,
+      choseindex: 0,
+      open: false,
+      information: {
+        sex: 0,
+        birthday: ''
+      }
     }
   },
   beforeMount () {
@@ -87,6 +182,17 @@ export default {
       if (this.step > to) {
         this.step = to
       }
+    },
+    choserole (index) {
+      this.choseindex = index
+      // auto go next step
+      // this.step += 1
+    },
+    changebirthday (date) {
+      this.information.birthday = date
+    },
+    chosebirthday () {
+      this.open = !this.open
     }
   }
 }
@@ -123,17 +229,54 @@ export default {
     overflow: hidden;
     padding: 15px 0;
     margin-bottom: 50px;
-    img{
-      margin-top: 100px;
-      display: inline-block;
+    .item{
+      position: relative;
       width: 150px;
       height: 150px;
       border-radius: 100%;
+      overflow: hidden;
+      margin: auto;
+      img{
+        width: 100%;
+        height: 100%;
+        display: block;
+      }
+      p{
+        margin-bottom: 100px;
+        font-size: 18px;
+      }
+      .mask{
+        transition: all .3s linear;
+        opacity: 0;
+        position: absolute;
+        left: 0;
+        top: 0;
+        height: 100%;
+        width: 100%;
+        background: rgba(0,0,0,.6);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        &.active{
+          opacity: 1;
+        }
+        .ivu-icon{
+          font-size: 40px;
+          color: #fff;
+        }
+      }
     }
+  }
+}
+.information{
+  width: 40%;
+  margin: 0 auto;
+  .field{
     p{
-      margin-bottom: 100px;
-      font-size: 18px;
+      text-align: right;
+      line-height: 32px;
     }
+    margin: 10px 0;
   }
 }
 
